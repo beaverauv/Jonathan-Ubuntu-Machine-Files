2466b2dfb1b1931c46f0b2107a44d5e5c2eebc2d
fascettij <44498011+fascettij@users.noreply.github.com>
2019-08-07 18:04:29 -0400

    Add files via upload

--- catkin_generated/pkg.develspace.context.pc.py
+++ catkin_generated/pkg.develspace.context.pc.py
@@ -1,8 +1,8 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jonathanfascetti/catkin_ws/src/imu_vn_100/include;/home/jonathanfascetti/catkin_ws/src/imu_vn_100/vncpplib/include;/usr/include".split(';') if "/home/jonathanfascetti/catkin_ws/src/imu_vn_100/include;/home/jonathanfascetti/catkin_ws/src/imu_vn_100/vncpplib/include;/usr/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "diagnostic_updater;roscpp;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-limu_vn_100".split(';') if "-limu_vn_100" != "" else []
-PROJECT_NAME = "imu_vn_100"
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "autofocus"
 PROJECT_SPACE_DIR = "/home/jonathanfascetti/catkin_ws/devel"
 PROJECT_VERSION = "0.0.0"
